6e1f5261b514bbd158d3a828a81e52e06fe4fe4d
LiuNikol <109183686+LiuNikol@users.noreply.github.com>
2023-08-27 03:56:53 +0300

    hw_23_lesson 30 +   @Test                    public void testhw() {                        List<String> dimension = when()                                .get("https://rickandmortyapi.com/api/location").then().extract().jsonPath().getList("results.dimension", String.class);                        System.out.println(dimension);                    }

---
+++ src/main/java/jsonObject/Result.java
@@ -0,0 +1,20 @@
+package jsonObject;
+
+import javax.lang.model.util.Elements;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Result {
+    public int id;
+    public String name;
+    public String status;
+    public String species;
+    public String type;
+    public String gender;
+    public Origin origin;
+    public Location location;
+    public String image;
+    public ArrayList<String> episode;
+    public String url;
+    public Date created;
+}
---
+++ src/main/java/jsonObject/location/Locations.java
@@ -0,0 +1,8 @@
+package jsonObject.location;
+
+import java.util.ArrayList;
+
+public class Locations {
+    public
+    Info info;
+    public ArrayList<Result> results;}
